a029fad7c8c530a4249fa6f16fcba37283c073c6
DaisyBit <DaisyBit@users.noreply.github.com>
2026-02-03 12:46:10 -0500

    Started options menu and loading menu and added custom cursors

---
+++ Assets/_Project/Scripts/Menu Scripts/CursorButton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CursorButton : MonoBehaviour
+{
+    public int cursorID; // 1, 2, or 3
+
+    public void SelectCursor()
+    {
+        if (CursorManager.Instance == null)
+        {
+            Debug.LogWarning("CursorManager not found!");
+            return;
+        }
+
+        if (cursorID == 1) CursorManager.Instance.SetCursor1();
+        else if (cursorID == 2) CursorManager.Instance.SetCursor2();
+        else if (cursorID == 3) CursorManager.Instance.SetCursor3();
+    }
+}
+
---
+++ Assets/_Project/Scripts/Menu Scripts/CursorManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CursorManager : MonoBehaviour
+{
+    public static CursorManager Instance;
+
+    public Texture2D cursor1;
+    public Texture2D cursor2;
+    public Texture2D cursor3;
+
+    private Vector2 hotspot = Vector2.zero;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+
+    void Start()
+    {
+        LoadSavedCursor();
+    }
+
+    public void SetCursor1()
+    {
+        Cursor.SetCursor(cursor1, hotspot, CursorMode.Auto);
+        PlayerPrefs.SetInt("CursorChoice", 1);
+    }
+
+    public void SetCursor2()
+    {
+        Cursor.SetCursor(cursor2, hotspot, CursorMode.Auto);
+        PlayerPrefs.SetInt("CursorChoice", 2);
+    }
+
+    public void SetCursor3()
+    {
+        Cursor.SetCursor(cursor3, hotspot, CursorMode.Auto);
+        PlayerPrefs.SetInt("CursorChoice", 3);
+    }
+
+    void LoadSavedCursor()
+    {
+        int saved = PlayerPrefs.GetInt("CursorChoice", 1);
+
+        if (saved == 1) SetCursor1();
+        else if (saved == 2) SetCursor2();
+        else if (saved == 3) SetCursor3();
+    }
+}
